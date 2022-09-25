eed6780d81f23eb1b94d02f10817fb78786c2fb1
akiskowitz <iskowan@ssfs.org>
2022-09-24 21:56:23 -0400

    commit_save_4

---
+++ assignment_two.py
@@ -0,0 +1,32 @@
+# asking user their name
+userName = input("What is your name?:")
+
+# printing and introducing Chatbot
+print("Hi", userName,"! I'm Chatbot")
+
+# asking where user is from
+userLocation = input("Where are your from?:")
+
+# complementing users location
+print(userLocation, "is actually on my bucketlist of places I want to go!")
+
+# asking user what their favorite number is
+userNumber = input("What's your favorite number?:")
+
+#defining Chatbot's favorite number
+chatNumber = float(userNumber) * 2
+# relating to the users favorite number
+print("Your favorite number", userNumber, "is 2 times smaller than my favorite number", chatNumber)
+
+# ask users dream home
+userHome = input("What's your dream home?:")
+print("I've also been longing for a", userHome, "too.")
+# aking the cost of the users dream home
+userHomeCost = input("How much does your dream home cost?:")
+print("Wow, $", userHomeCost, "That's expensive!")
+
+# P = float(userHomeCost)
+# n = monthlyPayments
+# r = intrest rate/100/12(12 months)
+
+# Equation = (r * P)/(1-(1+r)**-n)
