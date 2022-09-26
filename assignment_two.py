b9b1b7f99867b06832e2171aa8feda73b51dd96e
akiskowitz <iskowan@ssfs.org>
2022-09-26 15:04:02 -0400

    commit_save_7

--- assignment_two.py
+++ assignment_two.py
@@ -16,18 +16,16 @@ userNumber = input("What's your favorite number?:")
 #defining Chatbot's favorite number
 chatNumber = float(userNumber) * 2
 # relating to the users favorite number
-print("Your favorite number", userNumber, "is 2 times smaller than my favorite number", chatNumber)
+print("Your favorite numberis ", userNumber, "?! My favorite number is two times your favorite number", chatNumber)
 
 # ask users dream home
 userHome = input("What's your dream home?:")
-print("I've also been wanting a", userHome, "too.")
+print("I also want a", userHome, ".")
 # asking the cost of the users dream home
 userHomeCost = input("How much does your dream home cost?:")
-print("Wow, $", userHomeCost, "That's expensive!")
+print("Wow, $", userHomeCost, "! Is that in your budget?")
 
-monthlyPayments = (r * P)/(1-(1+r)**-n)
 P = float(userHomeCost)
 n =
 r = intrest rate/100/12 #(12 months)
-
-
+monthlyPayments = (r * P)/(1-(1+r)**-n)
